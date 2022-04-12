3ad5b2ba5cfef663c2a6a091415f53df0cc9b282
BShark1985 <leonand1985@gmail.com>
2022-04-12 13:56:54 +0300

    Добавьте файлы проекта.

---
+++ ITMO.124-16.Leontev.Algorithm6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO._124_16.Leontev.Algorithm6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 24;
+            int S = 0;
+            int T;
+            int Mid;
+            int i = 1;
+            for (; i <= n; i++) 
+            {
+                Console.WriteLine("Введите T");
+                T = int.Parse(Console.ReadLine());
+                S += T;
+            }
+            Mid = S / (i - 1);
+            Console.WriteLine("Mid = " + Mid);
+        }
+    }
+}
